798c70b00f7b36e33ce01b8138ec5987a55176fa
BaconCatBug <BaconCatBug@users.noreply.github.com>
2019-07-26 18:26:51 +0100

    Update Github.bat

--- Github.bat
+++ Github.bat
@@ -3,6 +3,6 @@ for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "d
 set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
-rem git add *.csv
+git add *.csv
 git commit -m %fullstamp%
-rem git push
+git push

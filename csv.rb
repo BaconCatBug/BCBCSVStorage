df6375cb64584afd15d18833ed2a83edc775c2ef
Giorgio Rossi <Giorgio.Rossi.2015@mumail.ie>
2019-09-28 19:12:05 +0100

    x

--- csv.rb
+++ csv.rb
@@ -7,6 +7,7 @@ sheet_gid_CM= "1373487754"
 sheet_gid_RL= "1623354916"
 sheet_gid_AB= "1933803309" 
 sheet_gid_MG= "195881060"
+sheet_gid_LM= "1881349203"
 
 buffer = open("https://docs.google.com/spreadsheets/d/#{google_sheet_id}/export?format=csv&gid=#{sheet_gid_SB}").read
 numCards = CSV.parse(buffer).length - 1 #CSV counts the header row; we only want the number of cards.
@@ -39,6 +40,14 @@ end
 buffer = open("https://docs.google.com/spreadsheets/d/#{google_sheet_id}/export?format=csv&gid=#{sheet_gid_MG}").read
 numCards = CSV.parse(buffer).length - 1 #CSV counts the header row; we only want the number of cards.
 File.open("C:\\Users\\BaconCatBug\\Documents\\GitHub\\BCBCSVStorage\\Magicite.csv", 'wb') do |file|
+    file << buffer
+
+end
+
+
+buffer = open("https://docs.google.com/spreadsheets/d/#{google_sheet_id}/export?format=csv&gid=#{sheet_gid_LM}").read
+numCards = CSV.parse(buffer).length - 1 #CSV counts the header row; we only want the number of cards.
+File.open("C:\\Users\\BaconCatBug\\Documents\\GitHub\\BCBCSVStorage\\LegendMateria.csv", 'wb') do |file|
     file << buffer
 
 end

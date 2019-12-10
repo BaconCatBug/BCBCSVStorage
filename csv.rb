7222c2cdb035c1058aa9d92ba1b85bcb0efe3d92
BaconCatBug <BaconCatBug@gmail.com>
2019-12-10 21:58:06 +0000

    'x'

--- csv.rb
+++ csv.rb
@@ -1,56 +1,11 @@
 require 'open-uri'
 require 'csv'
 
-google_sheet_id = "1f8OJIQhpycljDQ8QNDk_va1GJ1u7RVoMaNjFcHH0LKk"
-sheet_gid_SB= "344457459"
-sheet_gid_CM= "1373487754"
-sheet_gid_RL= "1623354916"
-sheet_gid_AB= "1933803309" 
-sheet_gid_MG= "195881060"
-sheet_gid_LM= "1881349203"
-
-buffer = open("https://docs.google.com/spreadsheets/d/#{google_sheet_id}/export?format=csv&gid=#{sheet_gid_SB}").read
-File.open("C:\\Users\\BaconCatBug\\Documents\\GitHub\\BCBCSVStorage\\SoulBreaks.csv", 'wb') do |file|
-    file << buffer
-end
-
-buffer = open("https://docs.google.com/spreadsheets/d/#{google_sheet_id}/export?format=csv&gid=#{sheet_gid_CM}").read
-File.open("C:\\Users\\BaconCatBug\\Documents\\GitHub\\BCBCSVStorage\\Commands.csv", 'wb') do |file|
-    file << buffer
-
-end
-
-buffer = open("https://docs.google.com/spreadsheets/d/#{google_sheet_id}/export?format=csv&gid=#{sheet_gid_RL}").read
-File.open("C:\\Users\\BaconCatBug\\Documents\\GitHub\\BCBCSVStorage\\Relics.csv", 'wb',) do |file|
-    file << buffer
-
-end
-
-
-buffer = open("https://docs.google.com/spreadsheets/d/#{google_sheet_id}/export?format=csv&gid=#{sheet_gid_AB}").read
-File.open("C:\\Users\\BaconCatBug\\Documents\\GitHub\\BCBCSVStorage\\Abilities.csv", 'wb') do |file|
-    file << buffer
-
-end
-
-buffer = open("https://docs.google.com/spreadsheets/d/#{google_sheet_id}/export?format=csv&gid=#{sheet_gid_MG}").read
-File.open("C:\\Users\\BaconCatBug\\Documents\\GitHub\\BCBCSVStorage\\Magicite.csv", 'wb') do |file|
-    file << buffer
-
-end
-
-
-buffer = open("https://docs.google.com/spreadsheets/d/#{google_sheet_id}/export?format=csv&gid=#{sheet_gid_LM}").read
-File.open("C:\\Users\\BaconCatBug\\Documents\\GitHub\\BCBCSVStorage\\LegendMateria.csv", 'wb') do |file|
-    file << buffer
-
-end
-
 sleep(2)
 time = Time.now.utc.strftime("%Y-%m-%d-%H:%M")
 puts 'python'
-system 'python StripColumns.py'
-system 'python FFRKDataFormat.py'
+system 'python FFRK_Soul_Break_etc_Data_Format.py'
+system 'python FFRK_Relic_Data_Format.py'
 puts 'add'
 system 'git add *.csv'
 puts 'commit'

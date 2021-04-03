04f6e171e16c4549d22d805ebf1eefae3bacdd4e
BaconCatBug <BaconCatBug@users.noreply.github.com>
2021-04-03 20:07:19 +0100

    2021-04-03

--- FFRK_Soul_Break_etc_Data_Format.py
+++ FFRK_Soul_Break_etc_Data_Format.py
@@ -12,6 +12,7 @@ sheet_gid_AB= "1933803309"
 sheet_gid_MG= "195881060"
 sheet_gid_LM= "1881349203"
 sheet_gid_LB= "258757653"
+sheet_gid_SY= "13552509"
 
 
 url = 'https://docs.google.com/spreadsheets/d/' + google_sheet_id + '/export?format=csv&gid=' + sheet_gid_AB
@@ -58,3 +59,12 @@ df_import.drop('Img', axis=1, inplace=True)
 df_import.drop(df_import.tail(16).index, inplace = True) 
 
 df_import.to_csv('LimitBreaks.csv', index=None, header=True, quoting=QUOTE_ALL)
+
+
+url = 'https://docs.google.com/spreadsheets/d/' + google_sheet_id + '/export?format=csv&gid=' + sheet_gid_SY
+df_import = read_csv(url, dtype=str)
+df_import.drop(df_import.columns[len(df_import.columns)-1], axis=1, inplace=True)
+df_import.drop(df_import.columns[len(df_import.columns)-1], axis=1, inplace=True)
+df_import.drop('Img', axis=1, inplace=True)
+
+df_import.to_csv('SyncCommands.csv', index=None, header=True, quoting=QUOTE_ALL)

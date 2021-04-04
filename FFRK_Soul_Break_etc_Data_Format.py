d565bf3a3d4ab2cda38e01a9f12602caea4cc862
BaconCatBug <BaconCatBug@users.noreply.github.com>
2021-04-04 03:50:19 +0100

    2021-04-04

--- FFRK_Soul_Break_etc_Data_Format.py
+++ FFRK_Soul_Break_etc_Data_Format.py
@@ -6,13 +6,15 @@ from pandas import read_csv, to_numeric, concat
 google_sheet_id = '1f8OJIQhpycljDQ8QNDk_va1GJ1u7RVoMaNjFcHH0LKk'
 # Soul Breaks
 
-sheet_gid_SB= "344457459"
-sheet_gid_CM= "1373487754"
-sheet_gid_AB= "1933803309" 
-sheet_gid_MG= "195881060"
-sheet_gid_LM= "1881349203"
-sheet_gid_LB= "258757653"
-sheet_gid_SY= "13552509"
+sheet_gid_SB = "344457459"
+sheet_gid_CM = "1373487754"
+sheet_gid_AB = "1933803309" 
+sheet_gid_MG = "195881060"
+sheet_gid_LM = "1881349203"
+sheet_gid_LB = "258757653"
+sheet_gid_SY = "13552509"
+sheet_gid_other = "2001933731"
+sheet_gid_status = "1899148923"
 
 
 url = 'https://docs.google.com/spreadsheets/d/' + google_sheet_id + '/export?format=csv&gid=' + sheet_gid_AB
@@ -58,15 +60,18 @@ df_import.drop(df_import.columns[len(df_import.columns)-1], axis=1, inplace=True
 df_import.drop(df_import.columns[len(df_import.columns)-1], axis=1, inplace=True)
 df_import.drop('Img', axis=1, inplace=True)
 df_import.drop(df_import.tail(16).index, inplace = True) 
-
 df_import.to_csv('LimitBreaks.csv', index=None, header=True, quoting=QUOTE_ALL)
 
 
-url = 'https://docs.google.com/spreadsheets/d/' + google_sheet_id + '/export?format=csv&gid=' + sheet_gid_SY
+url = 'https://docs.google.com/spreadsheets/d/' + google_sheet_id + '/export?format=csv&gid=' + sheet_gid_other
 df_import = read_csv(url, dtype=str)
 df_import.drop(df_import.columns[len(df_import.columns)-1], axis=1, inplace=True)
 df_import.drop(df_import.columns[len(df_import.columns)-1], axis=1, inplace=True)
-df_import["Element"].replace({"-":"NE"},inplace=True)
-df_import.drop('Img', axis=1, inplace=True)
+df_import.drop(df_import.columns[2], axis=1, inplace=True)
+df_import.to_csv('Other.csv', index=None, header=True, quoting=QUOTE_ALL)
 
-df_import.to_csv('SyncCommands.csv', index=None, header=True, quoting=QUOTE_ALL)
+url = 'https://docs.google.com/spreadsheets/d/' + google_sheet_id + '/export?format=csv&gid=' + sheet_gid_status
+df_import = read_csv(url, dtype=str)
+df_import.drop(df_import.columns[len(df_import.columns)-1], axis=1, inplace=True)
+df_import.drop(df_import.columns[len(df_import.columns)-1], axis=1, inplace=True)
+df_import.to_csv('Status.csv', index=None, header=True, quoting=QUOTE_ALL)

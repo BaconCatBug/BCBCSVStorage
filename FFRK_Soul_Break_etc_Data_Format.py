b381d88d838b480d4f5a640689ca2f6b229482d7
BaconCatBug <BaconCatBug@users.noreply.github.com>
2021-04-04 04:41:56 +0100

    2021-04-04

--- FFRK_Soul_Break_etc_Data_Format.py
+++ FFRK_Soul_Break_etc_Data_Format.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from csv import QUOTE_ALL
 from pandas import read_csv
+from time import sleep
 
 google_sheet_id = '1f8OJIQhpycljDQ8QNDk_va1GJ1u7RVoMaNjFcHH0LKk'
 # Soul Breaks
@@ -22,6 +23,7 @@ df_import.drop(df_import.columns[len(df_import.columns)-1], axis=1, inplace=True
 df_import.drop(df_import.columns[len(df_import.columns)-1], axis=1, inplace=True)
 df_import.to_csv('Abilities.csv', index=None, header=True, quoting=QUOTE_ALL)
 
+sleep(1)
 
 url = 'https://docs.google.com/spreadsheets/d/' + google_sheet_id + '/export?format=csv&gid=' + sheet_gid_CM
 df_import = read_csv(url, dtype=str)
@@ -30,6 +32,7 @@ df_import.drop(df_import.columns[len(df_import.columns)-1], axis=1, inplace=True
 df_import["Element"].replace({"-":"NE"},inplace=True)
 df_import.to_csv('Commands.csv', index=None, header=True, quoting=QUOTE_ALL)
 
+sleep(1)
 
 url = 'https://docs.google.com/spreadsheets/d/' + google_sheet_id + '/export?format=csv&gid=' + sheet_gid_LM
 df_import = read_csv(url, dtype=str)
@@ -37,6 +40,7 @@ df_import.drop(df_import.columns[len(df_import.columns)-1], axis=1, inplace=True
 df_import.drop(df_import.columns[len(df_import.columns)-1], axis=1, inplace=True)
 df_import.to_csv('LegendMateria.csv', index=None, header=True, quoting=QUOTE_ALL)
 
+sleep(1)
 
 url = 'https://docs.google.com/spreadsheets/d/' + google_sheet_id + '/export?format=csv&gid=' + sheet_gid_MG
 df_import = read_csv(url, dtype=str)
@@ -44,6 +48,7 @@ df_import.drop(df_import.columns[len(df_import.columns)-1], axis=1, inplace=True
 df_import.drop(df_import.columns[len(df_import.columns)-1], axis=1, inplace=True)
 df_import.to_csv('Magicite.csv', index=None, header=True, quoting=QUOTE_ALL)
 
+sleep(1)
 
 url = 'https://docs.google.com/spreadsheets/d/' + google_sheet_id + '/export?format=csv&gid=' + sheet_gid_SB
 df_import = read_csv(url, dtype=str)
@@ -52,6 +57,7 @@ df_import.drop(df_import.columns[len(df_import.columns)-1], axis=1, inplace=True
 df_import.drop(df_import.columns[len(df_import.columns)-1], axis=1, inplace=True)
 df_import.to_csv('SoulBreaks.csv', index=None, header=True, quoting=QUOTE_ALL)
 
+sleep(1)
 
 url = 'https://docs.google.com/spreadsheets/d/' + google_sheet_id + '/export?format=csv&gid=' + sheet_gid_LB
 df_import = read_csv(url, dtype=str)
@@ -61,6 +67,7 @@ df_import.drop('Img', axis=1, inplace=True)
 df_import.drop(df_import.tail(16).index, inplace = True) 
 df_import.to_csv('LimitBreaks.csv', index=None, header=True, quoting=QUOTE_ALL)
 
+sleep(1)
 
 url = 'https://docs.google.com/spreadsheets/d/' + google_sheet_id + '/export?format=csv&gid=' + sheet_gid_other
 df_import = read_csv(url, dtype=str)
@@ -68,6 +75,8 @@ df_import.drop(df_import.columns[len(df_import.columns)-1], axis=1, inplace=True
 df_import.drop(df_import.columns[len(df_import.columns)-1], axis=1, inplace=True)
 df_import.drop(df_import.columns[2], axis=1, inplace=True)
 df_import.to_csv('Other.csv', index=None, header=True, quoting=QUOTE_ALL)
+
+sleep(1)
 
 url = 'https://docs.google.com/spreadsheets/d/' + google_sheet_id + '/export?format=csv&gid=' + sheet_gid_status
 df_import = read_csv(url, dtype=str)

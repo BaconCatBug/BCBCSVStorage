c26209dc64e1e965a8d695bd36c95f6ff7183c8b
BaconCatBug <BaconCatBug@users.noreply.github.com>
2021-04-10 19:38:29 +0100

    Script Update

--- RunScripts.py
+++ RunScripts.py
@@ -6,9 +6,9 @@ now = str(now.year) +'-'+ f"{now.month:02d}" +'-'+ f"{now.day:02d}"
 # print(now)
 
 # print('FFRK_Relic_Data_Format')
-#system('python FFRK_Relic_Data_Format.py')
+system('python FFRK_Relic_Data_Format.py')
 # print('FFRK_Soul_Break_etc_Data_Format')
-#system('python FFRK_Soul_Break_etc_Data_Format.py')
+system('python FFRK_Soul_Break_etc_Data_Format.py')
 # print('git add *')
 system('git add *')
 # print("git commit -m 'now'")

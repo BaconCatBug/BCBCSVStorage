deff08aa0cb197d7c1f06f3df3f6b76efd016d0c
BaconCatBug <BaconCatBug@users.noreply.github.com>
2021-12-03 00:33:39 +0000

    2021-12-03

--- Warframe/WarframeScripts.py
+++ Warframe/WarframeScripts.py
@@ -82,9 +82,9 @@ try:
 
     for x in range(0, retry_attempts):
         try:
-            headers = {"Range": "bytes=0-1000"}  # first 100 bytes
-            soup = str(BeautifulSoup(get(url_relics,headers=headers).content, "lxml")).replace('\n', '')
-            print(soup)
+            #headers = {"Range": "bytes=0-1000"}  # first 100 bytes
+            #soup = str(BeautifulSoup(get(url_relics,headers=headers).content, "lxml")).replace('\n', '')
+            soup = str(BeautifulSoup(get(url_relics).content, "lxml")).replace('\n', '')
             #print('Saving Local Data')
             #with open(relic_data_txt_name, 'w') as f:
             #    f.write(soup)
@@ -92,6 +92,7 @@ try:
         except Exception:
             print('Relic data download failed, retrying... ' + str(retry_attempts - x - 1) + ' attempts left...', end='\r')
 
+    print(soup)
 
     print('Relic Data Downloaded')
     print('Processing Relic Data')

620d0ec7128ad0d801aae71b9b3591fe09c80591
BaconCatBug <BaconCatBug@users.noreply.github.com>
2021-12-03 16:10:15 +0000

    2021-12-03

--- Warframe/WarframeCSVtoTSV.py
+++ Warframe/WarframeCSVtoTSV.py
@@ -7,14 +7,14 @@ with open('DayPrices.csv','r') as csvin, open('DayPrices.txt', 'w') as tsvout:
     for row in csvin:
         tsvout.writerow(row)
         
- with open('HourPrices.csv','r') as csvin, open('HourPrices.txt', 'w') as tsvout:
+with open('HourPrices.csv','r') as csvin, open('HourPrices.txt', 'w') as tsvout:
     csvin = csv.reader(csvin)
     tsvout = csv.writer(tsvout, delimiter='\t')
 
     for row in csvin:
         tsvout.writerow(row)
         
- with open('Prime-Relic Data.csv','r') as csvin, open('Prime-Relic Data.txt', 'w') as tsvout:
+with open('Prime-Relic Data.csv','r') as csvin, open('Prime-Relic Data.txt', 'w') as tsvout:
     csvin = csv.reader(csvin)
     tsvout = csv.writer(tsvout, delimiter='\t')
 

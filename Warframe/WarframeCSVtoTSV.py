c1b8dc034448eb38f0b65d98fa836b21c3167b30
BaconCatBug <BaconCatBug@users.noreply.github.com>
2021-12-07 22:01:31 +0000

    2021-12-07

--- Warframe/WarframeCSVtoTSV.py
+++ Warframe/WarframeCSVtoTSV.py
@@ -2,7 +2,7 @@ import csv
 from datetime import datetime
 
         
-with open('HourPrices.csv','r') as csvin, open('HourPrices.txt', 'w') as tsvout:
+with open('DayPrices.csv','r') as csvin, open('DayPrices.txt', 'w') as tsvout:
     csvin = csv.reader(csvin)
     tsvout = csv.writer(tsvout, delimiter='\t')
 
--- Warframe/WarframeScripts.py
+++ Warframe/WarframeScripts.py
@@ -54,24 +54,27 @@ try:
 
 
     # Reads and sanitises the previous day data into a pandas dataframe
-    #df_previous_day = read_json(previous_day)
-    #df_previous_day = df_previous_day.drop(columns=['id', 'plat_worth', 'median'])
-    #df_previous_day = df_previous_day.rename(columns={'item': 'id'})
+    df_previous_day = read_json(previous_day)
+    df_previous_day = df_previous_day.drop(columns=['id', 'plat_worth', 'median'])
+    df_previous_day = df_previous_day.rename(columns={'item': 'id'})
     # Merges the item data and previous day data on the id column, drops the redundant id column, then renames the column names for export
-    #df_previous_day_merged = df_items.merge(df_previous_day, how='inner', on='id')
-    #df_previous_day_merged = df_previous_day_merged.drop(columns=['id'])
-    #df_previous_day_merged = df_previous_day_merged.reindex(columns=['item_name', 'datetime', 'ducats_per_platinum', 'ducats', 'wa_price','ducats_per_platinum_wa', 'position_change_month', 'position_change_week', 'position_change_day', 'volume'])
-    #df_previous_day_merged = df_previous_day_merged.sort_values(by='item_name')
-    #df_previous_day_merged['datetime'] = df_previous_day_merged['datetime'].astype(str).str[:-6]
-    #df_previous_day_merged = df_previous_day_merged.drop(columns=['datetime','ducats_per_platinum','position_change_month','position_change_week','position_change_day','volume'])
-    #df_previous_day_merged = df_previous_day_merged.reset_index(drop=True)
+    df_previous_day_merged = df_items.merge(df_previous_day, how='inner', on='id')
+    df_previous_day_merged = df_previous_day_merged.drop(columns=['id'])
+    df_previous_day_merged = df_previous_day_merged.reindex(columns=['item_name', 'datetime', 'ducats_per_platinum', 'ducats', 'wa_price','ducats_per_platinum_wa', 'position_change_month', 'position_change_week', 'position_change_day', 'volume'])
+    df_previous_day_merged = df_previous_day_merged.sort_values(by='item_name')
+    df_previous_day_merged['datetime'] = df_previous_day_merged['datetime'].astype(str).str[:-6]
+    df_previous_day_merged = df_previous_day_merged.drop(columns=['datetime','ducats_per_platinum','position_change_month','position_change_week','position_change_day','volume'])
+    filter_df = df_previous_hour_merged['item_name'].str.contains(patternDel)
+    df_previous_day_merged = df_previous_hour_merged[~filter_df]
+    df_previous_day_merged = df_previous_hour_merged.reset_index(drop=True)
+    df_previous_day_merged = df_previous_day_merged.reset_index(drop=True)
 
     # Reads and sanitises the previous hour data into a pandas dataframe
-    df_previous_hour = read_json(previous_hour)
-    df_previous_hour = df_previous_hour.drop(columns=['id', 'plat_worth', 'median'])
-    df_previous_hour = df_previous_hour.rename(columns={'item': 'id'})
+    #df_previous_hour = read_json(previous_hour)
+    #df_previous_hour = df_previous_hour.drop(columns=['id', 'plat_worth', 'median'])
+    #df_previous_hour = df_previous_hour.rename(columns={'item': 'id'})
     # Merges the item data and previous hour data on the id column, drops the redundant id column, then renames the column names for export
-    df_previous_hour_merged = df_items.merge(df_previous_hour, how='inner', on='id')
+    #df_previous_hour_merged = df_items.merge(df_previous_hour, how='inner', on='id')
 
     #df_items2 = df_items2.merge(df_previous_hour, how='inner', on='id')
     #patternDel = '.+ Set$'
@@ -81,15 +84,17 @@ try:
     #with option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
     #    print(df_items2)
     #pause()
-    df_previous_hour_merged = df_previous_hour_merged.drop(columns=['id'])
-    df_previous_hour_merged = df_previous_hour_merged.reindex(columns=['item_name', 'datetime', 'ducats_per_platinum', 'ducats', 'wa_price','ducats_per_platinum_wa', 'position_change_month', 'position_change_week', 'position_change_day', 'volume'])
-    df_previous_hour_merged = df_previous_hour_merged.sort_values(by='item_name')
-    df_previous_hour_merged['datetime'] = df_previous_hour_merged['datetime'].astype(str).str[:-6]
-    df_previous_hour_merged = df_previous_hour_merged.drop(columns=['datetime','ducats_per_platinum','position_change_month','position_change_week','position_change_day','volume'])
-    patternDel = '.+ Set$'
-    filter_df = df_previous_hour_merged['item_name'].str.contains(patternDel)
-    df_previous_hour_merged = df_previous_hour_merged[~filter_df]
-    df_previous_hour_merged = df_previous_hour_merged.reset_index(drop=True)
+    #df_previous_hour_merged = df_previous_hour_merged.drop(columns=['id'])
+    #df_previous_hour_merged = df_previous_hour_merged.reindex(columns=['item_name', 'datetime', 'ducats_per_platinum', 'ducats', 'wa_price','ducats_per_platinum_wa', 'position_change_month', 'position_change_week', 'position_change_day', 'volume'])
+    #df_previous_hour_merged = df_previous_hour_merged.sort_values(by='item_name')
+    #df_previous_hour_merged['datetime'] = df_previous_hour_merged['datetime'].astype(str).str[:-6]
+    #df_previous_hour_merged = df_previous_hour_merged.drop(columns=['datetime','ducats_per_platinum','position_change_month','position_change_week','position_change_day','volume'])
+    #patternDel = '.+ Set$'
+    #filter_df = df_previous_hour_merged['item_name'].str.contains(patternDel)
+    #df_previous_hour_merged = df_previous_hour_merged[~filter_df]
+    #df_previous_hour_merged = df_previous_hour_merged.reset_index(drop=True)
+    
+    
     print('Ducat Data Processed')
     print('Downloading Relic Data')
     url_relics = 'https://n8k6e2y6.ssl.hwcdn.net/repos/hnfvc0o3jnfvc873njb03enrf56.html'
@@ -174,8 +179,8 @@ try:
     # Export data
     print('Exporting Worksheet')
     df_even_more_parsed_relics.to_csv(csv_name, index=None, quoting=QUOTE_ALL)
-    #df_previous_day_merged.to_csv('DayPrices.csv', index=None, quoting=QUOTE_ALL)
-    df_previous_hour_merged.to_csv('HourPrices.csv', index=None, quoting=QUOTE_ALL)
+    df_previous_day_merged.to_csv('DayPrices.csv', index=None, quoting=QUOTE_ALL)
+    #df_previous_hour_merged.to_csv('HourPrices.csv', index=None, quoting=QUOTE_ALL)
     #with ExcelWriter(workbook_name, mode='a', engine='openpyxl', if_sheet_exists='replace') as writer:
     #    df_previous_day_merged.to_excel(writer, sheet_name=sheet_name_day)
     #    df_previous_hour_merged.to_excel(writer, sheet_name=sheet_name_hour)

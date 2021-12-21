f67799e6f4150b981c88db2b0ce48f40ebe290f2
Oracle Public Cloud User <opc@instance-20211221-1131.subnet12211008.vcn12211008.oraclevcn.com>
2021-12-21 11:43:46 +0000

    2021-12-21

---
+++ RunScriptsOracle.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from os import system, chdir
+
+now = datetime.now()
+now = str(now.year) +'-'+ str(now.month) +'-'+ str(now.day)
+
+chdir('/home/opc/BCBCSVStorage')
+# print('FFRK_Relic_Data_Format')
+system('python3 FFRK_Relic_Data_Format.py')
+# print('FFRK_Soul_Break_etc_Data_Format')
+system('python3 FFRK_Soul_Break_etc_Data_Format.py')
+# print('git add *.csv')
+system('git add *')
+# print("git commit -m 'now'")
+system('git commit -m "' + now + '"')
+# print('git push')
+system('git push')
